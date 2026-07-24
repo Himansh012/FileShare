fa03a46cbcf39bf625cc12339e62799e828bda5a
Himansh012 <devpathaksep12@gmail.com>
2026-07-24 14:24:02 +0530

    Working prototype of FileShare (uploads only)

--- app.py
+++ app.py
@@ -1,22 +1,43 @@
 from flask import Flask, render_template, request
-import pathlib
+from pathlib import Path
+from werkzeug.utils import secure_filename
+import time
 
 app = Flask(__name__)
 
+UPLOAD_FOLDER = Path("uploads")
+UPLOAD_FOLDER.mkdir(exist_ok=True)
+
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template(
+                            "index.html",
+                            files = [
+                                file 
+                                for file in UPLOAD_FOLDER.iterdir()
+                                  if file.is_file()
+                                    and not file.name.startswith(".")]
+                           )
 
 @app.route("/upload", methods = ["POST"])
 def upload():
-    # print(request.files)
-    uploaded_file = request.files["file"]
-    print(type(uploaded_file))
-    print(uploaded_file.filename)
-    print(uploaded_file.content_type)
+    uploaded_file = request.files.get("file")
+    if uploaded_file is None:
+        return "No file field found."
+    elif not uploaded_file.filename:
+        return "Please upload a file."
+    else:
+        filename = secure_filename(uploaded_file.filename)
+        filename = f"{int(time.time())}_{filename}"
 
-    print(dir(uploaded_file))
-    return home()
+        destination = UPLOAD_FOLDER / filename
+
+        uploaded_file.save(destination)
+
+    return render_template(
+                            "success.html",
+                            file_name = uploaded_file.filename
+                           )
 
 
 if __name__ == "__main__":
