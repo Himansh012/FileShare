72bf0d8bb0345fad6ff3fd8b6243d279e33abe22
Himansh012 <devpathaksep12@gmail.com>
2026-07-26 04:09:56 +0530

    List of uploaded files now appear on the website

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_file, abort
 from pathlib import Path
 from werkzeug.utils import secure_filename
 import time
@@ -15,8 +15,7 @@ def home():
                             files = [
                                 file 
                                 for file in UPLOAD_FOLDER.iterdir()
-                                  if file.is_file()
-                                    and not file.name.startswith(".")]
+                                  if file.is_file() and not file.name.startswith(".")]
                            )
 
 @app.route("/upload", methods = ["POST"])
@@ -24,20 +23,28 @@ def upload():
     uploaded_file = request.files.get("file")
     if uploaded_file is None:
         return "No file field found."
-    elif not uploaded_file.filename:
+    if not uploaded_file.filename:
         return "Please upload a file."
-    else:
-        filename = secure_filename(uploaded_file.filename)
-        filename = f"{int(time.time())}_{filename}"
+    
+    filename = secure_filename(uploaded_file.filename)
+    filename = f"{int(time.time())}_{filename}"
 
-        destination = UPLOAD_FOLDER / filename
-
-        uploaded_file.save(destination)
+    destination = UPLOAD_FOLDER / filename
+    uploaded_file.save(destination)
 
     return render_template(
                             "success.html",
                             file_name = uploaded_file.filename
                            )
+
+@app.route("/download/<filename>", methods = ["GET"])
+def download(filename):
+    destination = UPLOAD_FOLDER / filename
+    if not destination.is_file():
+        abort(404)
+    return send_file(destination,
+                     as_attachment=True)
+
 
 
 if __name__ == "__main__":
